679996f57035f505f06e06a53889c5acbb7e1137
Amos Kenner <akenner92@gmail.com>
2022-06-23 11:34:08 -0500

    Add hovering effect to grid

--- script.js
+++ script.js
@@ -7,8 +7,13 @@ function createGrid(size = 16) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", changeColor);
     grid.appendChild(square);
   }
+}
+
+function changeColor(e) {
+  e.target.style.backgroundColor = "#808080";
 }
 
 createGrid();
